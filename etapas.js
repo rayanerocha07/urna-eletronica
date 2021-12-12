c2db3476dbb9e161c6fd4701c926280151fe5ab7
Rayane Rocha <rayane.rocha07@gmail.com>
2021-12-11 22:14:50 -0300

    final updates

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '55896',
+        nome: 'Charles Leclerc',
+        partido: 'MONEGATO',
+        vice: '',
+        fotos: [{ url: 'images/Leclerc.jpg', legenda: 'Vereador' }],
+      },
+      {
+        numero: '28020',
+        nome: 'Daniel Ricciardo',
+        partido: 'EITA',
+        vice: '',
+        fotos: [{ url: 'images/Daniel.jpg', legenda: 'Vereador' }],
+      },
+    ],
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '55',
+        nome: 'Lewis Hamilton',
+        vice: 'Max Verstappen',
+        partido: 'TRETA',
+        fotos: [
+          { url: 'images/Lewis-Hamilton.jpg', legenda: 'Prefeito' },
+          {
+            url: 'images/VERSTAPPEN_.jpg',
+            legenda: 'Vice-prefeito',
+            small: true,
+          },
+        ],
+      },
+      {
+        numero: '79',
+        nome: 'Sebastian Vettel',
+        vice: 'Kimi Raikkonen',
+        partido: 'FIA',
+        fotos: [
+          { url: 'images/Vettel.jpg', legenda: 'Prefeito' },
+          { url: 'images/Kimi.jpg', legenda: 'Vice-prefeito', small: true },
+        ],
+      },
+    ],
+  },
+];
